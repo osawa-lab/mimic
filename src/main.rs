138c864ffcfbb93bf3a0f639e1ffbafa7d05a120
kento tajiri <k.denjiry@gmail.com>
2020-05-30 17:35:53 +0900

    init to main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Docs {
+    id: String,
+    content: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let s1 = "Hello, world!".to_string();
+    let s2 = "Hello, world!".to_string();
+    let s3 = "goodnight, world!".to_string();
+    let input_docs = vec![s1, s2, s3];
+    let cluster = make_cluster(&input_docs);
+    println!("{:?}", cluster);
+}
+
+fn make_cluster(docs: &Vec<String>) -> Vec<Docs> {
+    vec![Docs {
+        id: "s1234".to_string(),
+        content: docs[0].clone(),
+    }]
 }
