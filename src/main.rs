db4a41df8c23cab965bcbe58d29a2e6a64d62da3
kento tajiri <k.denjiry@gmail.com>
2020-06-20 17:50:47 +0900

    wakaran

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,17 @@ struct Docs {
 }
 
 fn main() {
-    let x = Exec::cmd("ls")
+    let dir: PathBuf = [env!("CARGO_MANIFEST_DIR"), "data"].iter().collect();
+    let command = format!("ls {}", dir.display());
+    let x = Exec::cmd(command)
         .stream_stdout()
         .expect("assume the command exists.");
-    let mut dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    dir.push("data");
+    let br = BufReader::new(x);
+    // for (i, line) in br.lines().enumerate() {
+    //     println!("{}: {}", i, line.unwrap());
+    // }
+
+    let files_under_dir: Vec<_> = br.lines().iter();
     // let file = File::open(filename).unwrap();
     // let reader = BufReader::new(&file);
 
