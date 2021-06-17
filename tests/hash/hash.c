cab0789d6121bcaa0277ee0e2f6d68aa35bb1ac3
Halno41220 <chka18049@g.nihon-u.ac.jp>
2021-06-17 16:37:02 +0900

    update hash files

--- tests/hash/hash.c
+++ tests/hash/hash.c
@@ -26,6 +26,7 @@ typedef struct openAdd_cell{
     int flag;
     char nickname[KEYL];
     char name[DATAL];
+    char skil[DATAL];
 } cell_oa_t;
 
 /**********************************************************************
@@ -34,6 +35,7 @@ typedef struct openAdd_cell{
 typedef struct chain_cell {
     char nickname[KEYL];
     char name[DATAL];
+    char skil[DATAL];
     struct chain_cell *next;
 } cell_ch_t;
 
@@ -47,20 +49,24 @@ int key_code_sum(char *s);
 /**********************************************************************
  * hash_A関数（内部ハッシュ法での実装ケース）に用いる関数（プロトタイプ宣言）
  **********************************************************************/
-int hash_A(char nicknames[][KEYL], char names[][DATAL], int data_n);
+int hash_A(char nicknames[][KEYL], char names[][DATAL], char skils[][DATAL], int data_n);
+void action_a(char *key, cell_oa_t **hashtbl);
 void init_table_a(cell_oa_t **hashtbl);
 int insert_table_a(cell_oa_t *r, cell_oa_t **hashtbl);
 cell_oa_t * search_table_a(char *key, cell_oa_t **hashtbl);
+int delete_data_a(cell_oa_t *p, cell_oa_t **hashtbl);
 void print_table_a(cell_oa_t **hashtbl);
 void hash_free_a(cell_oa_t **hashtbl);
 
 /**********************************************************************
  * hash_B関数（外部ハッシュ法での実装ケース）に用いる関数（プロトタイプ宣言）
  **********************************************************************/
-int hash_B(char nicknames[][KEYL], char names[][DATAL], int data_n);
+int hash_B(char nicknames[][KEYL], char names[][DATAL], char skils[][DATAL], int data_n);
+void action_b(char *key, cell_ch_t **hashtbl);
 void init_table_b(cell_ch_t **hashtbl);
 int insert_table_b(cell_ch_t *r, cell_ch_t **hashtbl);
 cell_ch_t * search_table_b(char *key, cell_ch_t **hashtbl);
+int delete_data_b(char *key, cell_ch_t **hashtbl);
 void print_table_b(cell_ch_t **hashtbl);
 void hash_free_b(cell_ch_t **hashtbl);
 void cell_free(cell_ch_t *cell);
@@ -72,6 +78,7 @@ void cell_free(cell_ch_t *cell);
 int main() {
     char nicknames[MAX_DATA_N][KEYL];
     char names[MAX_DATA_N][DATAL];
+    char skils[MAX_DATA_N][DATAL];
     int data_n = 0;
     
     char hash_f_type;
@@ -80,7 +87,7 @@ int main() {
     scanf("%c", &hash_f_type);
     
     printf("filename: ");
-    scanf("%s", fname);
+    scanf("%s", &fname);
     
     FILE *fp;
     fp = fopen(fname, "r");
@@ -91,16 +98,16 @@ int main() {
         printf("%s file opened!\n", fname);
     }
     
-    for(data_n = 0; fscanf(fp, " %[^,],%s", nicknames[data_n], names[data_n]) != EOF
+    for(data_n = 0; fscanf(fp, " %[^,],%[^,],%s", nicknames[data_n], names[data_n], skils[data_n]) != EOF
             && data_n < MAX_DATA_N ; data_n++){
         //printf("%s %s\n", nicknames[data_n], names[data_n]);
     }
     fclose(fp); // ファイルを閉じる
     
     if(hash_f_type=='a'){
-        hash_A(nicknames, names, data_n);
+        hash_A(nicknames, names, skils, data_n);
     }else if(hash_f_type=='b'){
-        hash_B(nicknames, names, data_n);
+        hash_B(nicknames, names, skils, data_n);
     }
    
     return 0;
@@ -140,7 +147,7 @@ int key_code_sum(char * s) {
 /**********************************************************************
  * hash_A関数に用いる関数（空欄箇所を実装）
  **********************************************************************/
-int hash_A(char nicknames[][KEYL], char names[][DATAL], int data_n) {
+int hash_A(char nicknames[][KEYL], char names[][DATAL], char skils[][DATAL], int data_n) {
     cell_oa_t *hashtbl[MAX];
 
     /*-★ ハッシュテーブルの初期設定 --*/
@@ -152,6 +159,7 @@ int hash_A(char nicknames[][KEYL], char names[][DATAL], int data_n) {
         c->flag = USED;
         strcpy(c->nickname, nicknames[i]);
         strcpy(c->name, names[i]);
+        strcpy(c->skil, skils[i]);
         if(!insert_table_a(c, hashtbl)) {
             printf ("hash_table overflow\n");
             exit(0);
@@ -159,26 +167,36 @@ int hash_A(char nicknames[][KEYL], char names[][DATAL], int data_n) {
     }
     print_table_a(hashtbl);
 
-    // キー kate を持つデータを表示
-    printf("search_data (Kate): ");
-    cell_oa_t * p = search_table_a("Kate", hashtbl);
-    p ? printf("%s\n", p->name) : printf("NULL\n");
-
-    // キー Larry を持つデータを表示
-    printf("search_data (Larry): ");
-    p = search_table_a("Larry", hashtbl);
-    p ? printf("%s\n", p->name) : printf("NULL\n");
+    // それぞれの人物の能力を使用
+    action_a("Kate", hashtbl);
+    action_a("Jeff", hashtbl);
+    action_a("Lucy", hashtbl);
+    action_a("Gil", hashtbl);
+    action_a("Sean", hashtbl);
 
     /*-★ メモリ領域を解放する --*/
     hash_free_a(hashtbl);
     return 0;
 }
 
+/*-★ 能力発動関数 --*/
+void action_a(char *key, cell_oa_t **hashtbl) {
+    cell_oa_t * p = search_table_a(key, hashtbl);
+    printf("search_name (%s): ", key);
+    if(p) {
+        printf("%s\n", p->name);
+        printf("勇者「%s!!」 %s 発動!!\n", p->name, p->skil);
+        delete_data_a(p, hashtbl);
+    } else {
+        printf("NULL\nMISS!!\n");
+    }
+}
+
 /*-★ 初期設定関数 --*/
 void init_table_a(cell_oa_t **hashtbl){
     for(int cnt = 0; cnt < MAX; cnt++) {
         cell_oa_t *p = malloc(sizeof(cell_oa_t));
-        p->flag = NIL; strcpy(p->nickname, ""); strcpy(p->name, "");
+        p->flag = NIL; strcpy(p->nickname, ""); strcpy(p->name, ""); strcpy(p->skil, "");
         hashtbl[cnt] = p;
     }
 }
@@ -214,19 +232,31 @@ cell_oa_t * search_table_a(char *key, cell_oa_t **hashtbl){
     while(p->flag != NIL && i < MAX){
         if(strcmp(key, p->nickname) == 0)
             return p;
-        else
-            ++i;
+        else {
             h = double_hash(key, h0, i);
             p = hashtbl[h];
+            ++i;
+        }
     }
     return NULL;
+}
+
+/*-★ データ削除関数 --*/
+int delete_data_a(cell_oa_t *p, cell_oa_t **hashtbl){
+    /* ノード自体を渡すため、今回は探索の手間がかからない */
+    p->flag = DEL;
+    strcpy(p->nickname, "");
+    strcpy(p->name, "");
+    strcpy(p->skil, "");
+    return 0;
 }
 
 /*-★ 内容表示関数 --*/
 void print_table_a(cell_oa_t **hashtbl) {
     for(int cnt = 0; cnt < MAX; cnt++){
         if(hashtbl[cnt]->flag != NIL){
-            printf("table[%d]: {%s:%s}\n", cnt, hashtbl[cnt]->nickname, hashtbl[cnt]->name);
+            printf("table[%d]: {%s:%s:%s}\n", 
+                cnt, hashtbl[cnt]->nickname, hashtbl[cnt]->name, hashtbl[cnt]->skil);
         }
     }
 }
@@ -250,7 +280,7 @@ void hash_free_a(cell_oa_t **hashtbl) {
 /**********************************************************************
  * hash_B関数に用いる関数（空欄箇所を実装）
  **********************************************************************/
-int hash_B(char nicknames[][KEYL], char names[][DATAL], int data_n) {
+int hash_B(char nicknames[][KEYL], char names[][DATAL], char skils[][DATAL], int data_n) {
     cell_ch_t *hashtbl[MAX];
 
     /*-★ ハッシュテーブルの初期設定 --*/
@@ -261,23 +291,34 @@ int hash_B(char nicknames[][KEYL], char names[][DATAL], int data_n) {
         if(c == NULL) exit(1);
         strcpy(c->nickname, nicknames[i]);
         strcpy(c->name, names[i]);
+        strcpy(c->skil, skils[i]);
         insert_table_b(c, hashtbl);
     }
     print_table_b(hashtbl);
 
-    // キー kate を持つデータを表示
-    printf("search_data (Kate): ");
-    cell_ch_t * p = search_table_b("Kate", hashtbl);
-    p ? printf("%s\n", p->name) : printf("NULL\n");
-
-    // キー Larry を持つデータを表示
-    printf("search_data (Larry): ");
-    p = search_table_b("Larry", hashtbl);
-    p ? printf("%s\n", p->name) : printf("NULL\n");
+    // それぞれの人物の能力を使用
+    action_b("Kate", hashtbl);
+    action_b("Jeff", hashtbl);
+    action_b("Rudy", hashtbl);
+    action_b("Gil", hashtbl);
+    action_b("Larry", hashtbl);
 
     /*-★ メモリ領域を解放する --*/
     hash_free_b(hashtbl);
     return 0;
+}
+
+/*-★ 能力発動関数 --*/
+void action_b(char *key, cell_ch_t **hashtbl) {
+    cell_ch_t * p = search_table_b(key, hashtbl);
+    printf("search_name (%s): ", key);
+    if(p) {
+        printf("%s\n", p->name);
+        printf("勇者「%s!!」 %s 発動!!\n", p->name, p->skil);
+        delete_data_b(p->nickname, hashtbl);
+    } else {
+        printf("NULL\nMISS!!\n");
+    }
 }
 
 /*-★ 初期設定関数 --*/
@@ -325,6 +366,44 @@ cell_ch_t * search_table_b(char *key, cell_ch_t **hashtbl){
     return NULL;
 }
 
+/*-★ データ削除関数 --*/
+int delete_data_b(char *key, cell_ch_t **hashtbl){
+    cell_ch_t *target = NULL;
+    cell_ch_t *chain  = NULL;
+    int hashval = hash(key);
+  
+    /* キーがハッシュテーブルに存在しているか確認する
+     * 基本的に存在するデータを削除する流れなので、今回は不要だが入れている。 
+     */
+    target = hashtbl[hashval];
+    if(target == NULL){
+        fprintf(stderr, "target[%s] is not exist in hash table.\n", key);
+        return -1;
+    }
+    chain = target->next;
+  
+    /* リストの先頭要素を削除する場合 */
+    if(strcmp(key, target->nickname) == 0){
+        hashtbl[hashval] = chain;
+        free(target);
+        return 0;
+    }
+  
+    /* 先頭以外の要素を削除する場合 */
+    while(target != NULL){
+        if(strcmp(key, target->nickname) == 0){
+            chain->next = target->next;
+            free(target);
+            return 0;
+        }
+        chain  = target;
+        target = target->next;
+    }
+  
+    return -1;
+}
+
+
 /*-★ 内容表示関数 --*/
 void print_table_b(cell_ch_t **hashtbl) {
     cell_ch_t *chain = NULL;
@@ -334,7 +413,8 @@ void print_table_b(cell_ch_t **hashtbl) {
             printf("table[%d]:", cnt);
             for(chain = hashtbl[cnt]; chain != NULL; chain = chain->next){
                 printf("->{%s:", (chain->nickname));
-                printf("%s}", (chain->name));
+                printf("%s:", (chain->name));
+                printf("%s}", (chain->skil));
             }
             putchar('\n');
         }
